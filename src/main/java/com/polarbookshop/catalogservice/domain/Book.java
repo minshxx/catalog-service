49c90cb651f90a4c8d11bd813c340d0e08cca811
minshxx <minseonghyeon@naver.com>
2025-01-21 21:59:50 +0900

    feat: Test코드 추가 단위테스트, 통합테스트, 클라이언트테스트, Json직렬화 테스트

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -3,7 +3,6 @@ package com.polarbookshop.catalogservice.domain;
 import lombok.Builder;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
@@ -11,7 +10,7 @@ import javax.validation.constraints.Positive;
 public record Book(
         @NotBlank(message = "The book ISBN must be defined.")
         @Pattern(
-                regexp = "^([0-9]{10}[0-9]{13})$",
+                regexp = "^([0-9]{10}|[0-9]{13})$",
                 message = "The ISBN format must be valid."
         )
         String isbn,
@@ -19,7 +18,6 @@ public record Book(
         String title,
         @NotBlank(message = "The book author must be defined.")
         String author,
-        @NotBlank(message = "The book price must be defined.")
         @Positive(message = "The book price must be greater than zero")
         Double price
 ) {
--- src/test/java/com/polarbookshop/catalogservice/ApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/ApplicationTests.java
@@ -1,13 +1,34 @@
 package com.polarbookshop.catalogservice;
 
+import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
 class ApplicationTests {
+    @Autowired private WebTestClient webTestClient;
 
     @Test
-    void contextLoads() {}
+    void whenPostRequestThenBookCreated() {
+        var expectedBook = new Book("1231231231", "Title", "Author", 9.90);
+
+        webTestClient
+                .post()
+                .uri("/books")
+                .bodyValue(expectedBook)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Book.class).value(actualBook -> {
+                    assertThat(actualBook).isNotNull();
+                    assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
+                });
+
+    }
 }
