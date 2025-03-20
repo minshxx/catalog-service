cab9a8d54ed517fa840dde0072d8e099ee1ac1bb
minshxx <minseonghyeon@naver.com>
2025-03-20 21:44:06 +0900

    Feat: properties클래스 추가
    Add PolarProperties.java

---
+++ src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
@@ -0,0 +1,16 @@
+package com.polarbookshop.catalogservice.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "polar")
+public class PolarProperties {
+
+    /**
+     * A message to welcome users.
+     */
+    private String greeting;
+}
